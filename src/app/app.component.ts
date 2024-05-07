15b62ff58883f767491a92dda2899f6af4b9f39e
Rajesh <ry292000@gmail.com>
2024-05-07 22:50:42 +0530

    Added Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'blog';
+  data = "code step by step";
+  getValue = () => {
+    return "GET VALUE"
+  }
 }
